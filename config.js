4ee33d8d2956b2d34a2a67b710d2a43ca0ca621b
Dr nhevic <irenembui408@gmail.com>
2024-07-27 13:40:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://gi
 global.caption = process.env.CAPTION || global.caption || "ᴘᴏᴡᴇʀᴇᴅ ʙʏ sᴜʜᴀɪʟ²²¹-ᴍᴅ" 
 
 
-global.devs = "254712611776" // Developer Contact
+global.devs = "254113566713" // Developer Contact
 global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "null";
 global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "2547113566713";
 
